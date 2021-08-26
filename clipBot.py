4de8408a3a9beebcc21b7dd3e7652430c7df5605
tanuarta <rtanuarta@gmail.com>
2021-08-26 21:06:09 +1000

    Implemented error messages for discord so the user has feedback on what is happening. There is now the alternative of uploading to Google Drive in case the bot is out of YouTube upload quotas.

--- clipBot.py
+++ clipBot.py
@@ -2,8 +2,11 @@ import discord
 from discord.ext import commands
 import logging
 import os, subprocess
-from googleapiclient.discovery import build
 import config
+from googleapiclient.discovery import build
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from uploadGdrive import upload
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -21,6 +24,10 @@ def author_check(author):
     return lambda ctx: ctx.author == author
 
 @bot.command()
+async def gdrive(ctx):
+    await ctx.send("https://drive.google.com/drive/u/1/folders/1SvBMA88yCqetZbf6ICxyZ47HkBX5n919")
+
+@bot.command()
 async def clip(ctx):
     video_types = ["mp4", "mov"]
 
@@ -29,64 +36,57 @@ async def clip(ctx):
         if any(attachment.filename.lower().endswith(video) for video in video_types):
             await attachment.save(attachment.filename)
 
+    
     await ctx.send("Please reply with a **title**")
     reply = await bot.wait_for("message", check=author_check(ctx.author), timeout=30)
     title = reply.content
     
-    await ctx.send("Please reply with **public**, **private** or **unlisted** for the video privacy settings, default is unlisted")
+    filename = attachment.filename
+
+    await ctx.send("Upload to youtube or google drive? (yt/gd)")
     reply = await bot.wait_for("message", check=author_check(ctx.author), timeout=30)
-    if "public" in reply.content:
-        privacy = "public"
-    elif "private" in reply.content:
-        privacy = "private"
+    if "gd" in reply.content:
+        drive_title = title + ".mp4"
+        os.rename(filename, drive_title)
+        await ctx.send('Uploading file. This can take a few minutes')
+
+        fileId = upload(drive_title)
+
+        await ctx.send('Uploaded file to {url}'.format(url='https://drive.google.com/open?id=' + fileId))
+
+        os.remove(drive_title)
+
     else:
-        privacy = "unlisted"
+        await ctx.send("Please reply with **public**, **private** or **unlisted** for the video privacy settings, default is unlisted")
+        reply = await bot.wait_for("message", check=author_check(ctx.author), timeout=30)
+        if "public" in reply.content:
+            privacy = "public"
+        elif "private" in reply.content:
+            privacy = "private"
+        else:
+            privacy = "unlisted"
 
-    await ctx.send("Uploading with title: **" + title + "**. This can take a few minutes")
-    
-    output = subprocess.check_output("youtube-upload --privacy='" + privacy + "' --title=\'" + title + "' " + attachment.filename, shell=True)
+        await ctx.send("Uploading with title: **" + title + "**. This can take a few minutes")
 
+        try:
+            output = subprocess.check_output("youtube-upload --privacy='" + privacy + "' --title=\'" + title + "' " + filename, shell=True)
+        except Exception as e:
+            output = e.output
 
-    msg = "Upload Complete: https://www.youtube.com/watch?v=" + output.decode("utf-8")
-    await ctx.message.channel.send(msg)
+        if output.decode("utf-8") == "":
+            await ctx.send("An error has occurred most likely I ran out of YT upload quotas, would you like to upload to Google Drive instead? (y/n)")
+            reply = await bot.wait_for("message", check=author_check(ctx.author), timeout=30)
+            if reply.content == "y":
+                await ctx.send("Beginning upload to Google Drive")
+                fileId = upload(filename, title)
+                await ctx.send('Uploaded file to {url}'.format(url='https://drive.google.com/open?id=' + fileId))
+            else:
+                await ctx.send("Please wait a few hours until my quota is refilled")
 
-    os.remove(attachment.filename)
-    return
-
-@bot.command()
-async def convo(ctx):
-    await ctx.send("hey i only respond to my author, what is my author name")
-    reply = await bot.wait_for("message", check=author_check(ctx.author), timeout=30)
-    await ctx.send("hi " + reply.content)
-
+        else:
+            msg = "Upload Complete: https://www.youtube.com/watch?v=" + output.decode("utf-8")
+            await ctx.send(msg)
+            
+        os.remove(filename)
 
 bot.run(config.discord_token)
-
-
-"""
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-    
-    if message.content.startswith('$hey'):
-        dude = message.author.id
-        await message.channel.send("i can only respond to my caller")
-        
-        @client.event
-        async def on_message(message):
-            if message.author == client.user:
-                return
-
-            if message.author.id == dude:
-                await message.channel.send("hi caller")
-            else:
-                await message.channel.send("you are not my caller")
-
-    
-    if message.content.startswith('$suck'):
-        await message.channel.send("sucky sucky")
-"""
---
+++ uploadGdrive.py
@@ -0,0 +1,41 @@
+from googleapiclient.http import MediaFileUpload
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from Google import Create_Service
+
+CLIENT_SECRET_FILE = 'client_secrets.json'
+API_NAME = 'drive'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+folder_id = '1SvBMA88yCqetZbf6ICxyZ47HkBX5n919'
+
+def upload(filename):
+    file_metadata = {
+        'name': filename,
+        'parents': [folder_id]
+    }
+
+    print("Uploading File")
+
+    media = MediaFileUpload(filename, mimetype='video/mp4')
+    file = service.files().create(  body=file_metadata,
+                                    media_body=media,
+                                    fields='id'
+                                ).execute()
+
+    print("File upload successful")
+
+    fileId = file.get('id')
+
+    permission = {
+            'type': 'anyone',
+            'role': 'reader',
+        }
+
+    service.permissions().create(fileId=fileId, body=permission).execute()
+    
+    return fileId
+    
